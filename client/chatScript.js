805d8984f890716116e9ee6f41be64d3254f1c45
YahyaDarwich <ydarwich18@gmail.com>
2022-12-31 20:25:44 +0200

    update server URL

--- client/chatScript.js
+++ client/chatScript.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   typingLoader(messageDiv)
 
-  const response = await fetch('https://codex-im0y.onrender.com/', {
+  const response = await fetch('https://codex-2au6.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
